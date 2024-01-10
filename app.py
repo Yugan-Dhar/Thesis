9638743a23a0c6bb3a3284fa31bdb1cec8b9a630
MikaSie <mikasie6@gmail.com>
2024-01-10 11:18:18 +0100

    changes

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ from summarizer import Summarizer
 from transformers import RobertaTokenizer, TFRobertaModel
 
 
-
-
-
 def get_pdf_text(pdf_docs, rect):
   """
   Takes pdf documents and returns raw texts.
@@ -34,7 +31,7 @@ def get_pdf_text(pdf_docs, rect):
   
 def get_text_chunks(raw_text, tokenizer):
 
-  #TODO: Create function which detects footers so they can be exluded
+  # CURRENTLY, FOOTNOTES ARE EXCLUDED BUT REFERENCES ARE TAKEN INTO CONSIDERATION THIS IS ALSO DONE IN EUR-LEX-SUM
   """ 
   Takes raw text and returns text chunks.
   
@@ -48,11 +45,10 @@ def get_text_chunks(raw_text, tokenizer):
   
   """
 
-
   text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
     tokenizer, 
-    chunk_size=505, 
-    chunk_overlap=50)
+    chunk_size = 505, 
+    chunk_overlap = 50)
   
   chunks = text_splitter.split_text(raw_text)
 
@@ -70,12 +66,15 @@ def extractive_summarization(chunk):
       summary(str): extractive summary of a piece of text
     """
     summarizer = Summarizer()
+
+    #TODO: Check ratio
+    #CAN ALSO MAKE USE OF CUSTOM MODEL, CHECK DOCUMENTATION OF SUMMARIZER. COULD BE USEFUL TO USE A LEGAL-BERT
     summary = summarizer(chunk, ratio=0.4)
 
     return summary
 
 
-def mark_text(summary, pdf_doc):
+def mark_text(summary, pdf_doc, rect):
   #TODO: Think of a method that marks the text 'logically'. Right now if '(f)' is extracted, if you hand it to the function. All (f)'s will be marked
   
    """
@@ -84,26 +83,30 @@ def mark_text(summary, pdf_doc):
    Parameters:
     summary(str): piece of text that is summarized.
 
-    pdf_doc: document that needs to be annotated
+    pdf_doc: document that needs to be annotated.
+
+    rect(fitz.rectangel): fitz rectangle that indicates where the text needs to be shown.
 
     Ouptut:
-      pdf file which has the sentences annotated
+      highlights on pdf file 
    """
-  
+
+   sentences_of_summary = nltk.sent_tokenize(summary)
+   
    for page in pdf_doc:
       ### SEARCH 
-      # Search is robust to capitalisation
-      text = "regulation of the european parliament and of the council"
-      text_instances = page.search_for(text)
+      # Search is robust to capitalisation 
 
-      ### HIGHLIGHT
-      for inst in text_instances:
-          highlight = page.add_highlight_annot(inst)
-          highlight.update()
+      for sentence in sentences_of_summary:
+        sentence = sentence.strip()
 
-  ### OUTPUT
-   pdf_doc.save("output.pdf", deflate=True, clean=True)
+        text_to_be_highlighted = page.search_for(sentence, clip = rect)
+        
+        #TODO: HIER MOET HET DEEL INKOMEN DAT DUS OPLET DAT NIET ALLES WORDT GEMARKEERD
 
+        for instance in text_to_be_highlighted:
+            highlight = page.add_highlight_annot(instance)
+            highlight.update()
 
 
 def main():
@@ -111,26 +114,28 @@ def main():
   #API TOKEN GEBRUIKEN
   tokenizer = RobertaTokenizer.from_pretrained('roberta-large')
 
-
   pdf_object = fitz.open("docs/test_pdf.pdf")
-  rect = fitz.Rect(10, 10, 585, 770) 
 
+  #TODO: USEFUL TO LET THE USER OF THE STREAMLITT APP CHANGE THE RECT SIZE 
+  rect = fitz.Rect(10, 40, 585, 770) 
 
   raw_text = get_pdf_text(pdf_object, rect) 
 
-
   #GET CHUNKS
   text_chunks = get_text_chunks(raw_text, tokenizer)
-  
 
-
-  for chunk in text_chunks[10:11]:
-      # Extractive Summarization
+  i = 1
+  for chunk in text_chunks:
+      # Perform extractive summarization
       summary = extractive_summarization(chunk)
-      print(summary)
-      # Mark Original Sentences
-      mark_text(summary, pdf_object)
 
+      print(f"Summary {i} completed")
+
+      i += 1
+      # Mark Original Sentences
+      mark_text(summary, pdf_object, rect)
+
+  pdf_object.save("output_quads.pdf", deflate=True, clean=True)
 
 if __name__ == '__main__':
   main()
