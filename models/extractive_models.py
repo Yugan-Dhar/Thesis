7e2234156fd8ffc787863183f5a6d21e31e4db92
MikaSie <mikasie6@gmail.com>
2024-02-21 10:41:51 +0100

    Changes to extr model
    Extractive models can now be laoded in easily

--- models/extractive_models.py
+++ models/extractive_models.py
@@ -1,25 +1,60 @@
-from transformers import AutoTokenizer, AutoModel, RobertaTokenizer, TFRobertaModel, AutoConfig
+from transformers import AutoTokenizer, AutoModel, AutoConfig
 from summarizer import Summarizer 
 
-#Every function returns a tokenizer and model which can be used to 
+
+#TODO: Add more models
+#TODO: Create a general function for all models, currently LegalBERT and RoBERTa are hardcoded as functions
 
 def LegalBERT():
+    """
+    Initializes the LegalBERT model for extractive summarization.
+
+    Returns:
+    - summarizer: The summarizer object for extractive summarization.
+    - tokenizer: The tokenizer object for LegalBERT.
+    """
     custom_config = AutoConfig.from_pretrained('nlpaueb/legal-bert-base-uncased')
     custom_config.output_hidden_states = True
     tokenizer = AutoTokenizer.from_pretrained("nlpaueb/legal-bert-base-uncased")
     model = AutoModel.from_pretrained("nlpaueb/legal-bert-base-uncased", config = custom_config)
 
-    summarizer = Summarizer(custom_model = model, custom_tokenizer= tokenizer)
-    print("done")
-    return tokenizer, summarizer
+    summarizer = Summarizer(custom_model = model, custom_tokenizer = tokenizer)
+    
+    return summarizer, tokenizer
 
+def RoBERTa():
+    """
+    Initializes the RoBERTa model for extractive summarization.
 
+    Returns:
+    - summarizer: The summarizer object for extractive summarization.
+    - tokenizer: The tokenizer object for RoBERTa.
+    """
+    custom_config = AutoConfig.from_pretrained('roberta-base')
+    custom_config.output_hidden_states = True
+    tokenizer = AutoTokenizer.from_pretrained('roberta-base')
+    model = AutoModel.from_pretrained('roberta-base', config = custom_config)
 
-
+    summarizer = Summarizer(custom_model = model, custom_tokenizer = tokenizer)
+    
+    return summarizer, tokenizer
 
 
 def select_extractive_model(model):
+    """
+    Selects and returns an extractive model based on the given model name.
+
+    Args:
+        model (str): The name of the model to select.
+
+    Returns:
+        object: An instance of the selected extractive model.
+    """
 
     if model == 'LegalBERT':
         return LegalBERT()
+    
+    elif model == 'RoBERTa':
+        return RoBERTa()
+
         
--- run_summarization.py
+++ run_summarization.py
@@ -1,52 +1,71 @@
-# import extractive and abstractive models 
-#from models.extractive_models import RoBERTa, LegalBERT
-#from models.abstractive_models import BARTSummarizer, T5Summarizer
 import models.extractive_models
+import os
+import warnings
+import warnings
+#Disable specific warning of SKLEARN because it is not relevant and also not fixable
+warnings.filterwarnings('ignore', category=FutureWarning, message='^The default value of `n_init` will change from 10 to \'auto\' in 1.4')
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 
 class ExtractiveSummarizationModel:
-    #TODO: Change this to more sophisticated. Probably implement it in extractive_models
+    #TODO: Possibly change this to something more sophisticated. Probably implement it in extractive_models but this makes it easier for readability to check where error comes from.
     model_classes = ['RoBERTa', 'LegalBERT']
 
     def __init__(self, model_type):
         self.model_type = model_type
         self.model, self.tokenizer= self.load_extractive_model()
 
-
+    
     def load_extractive_model(self):
         """
-        Loads the extractive model and its tokenizer based on the model type.
-
-        The method checks if the model type is valid (i.e., it exists in `model_classes`). 
-        If it's not valid, it raises a ValueError. If it is valid, it selects the model 
-        and its tokenizer using the `select_extractive_model` function from `models.extractive_models` 
-        and assigns them to `self.model` and `self.tokenizer` respectively.
-
-        Returns:
-            tuple: A tuple containing the loaded model and its tokenizer.
-
-        Raises:
-            ValueError: If the model type is not valid.
-        """
+        Loads the extractive model and its tokenizer based on the specified model type.
         
+        Returns:
+            model: The loaded extractive model.
+            tokenizer: The loaded tokenizer for the model.
+        """
         if self.model_type not in self.model_classes:
-            raise ValueError("Invalid extractive model type")
+            raise ValueError(f"Invalid extractive model type: {self.model_type}\nPlease select from {self.model_classes}")    
         
         self.model, self.tokenizer = models.extractive_models.select_extractive_model(self.model_type)
-
+        
+        print(f"Succesfully loaded {self.model_type} model and its tokenizer")
+        
         return self.model, self.tokenizer
 
 
     def summarize(self, text):
-        return self.model.summarize(text)
+        """
+        Summarizes the input text using the loaded extractive model.
 
+        Parameters:
+        text (str): The input text to be summarized.
+
+        Returns:
+        str: The extracted summary of the input text.
+        """
+
+        summary = self.model(text, ratio=0.4)
+
+        print(f"Extractive summary: \n----------------------\n{summary}\n----------------------\n")
+
+        return summary
+
+
+    #TODO: Finish this function
+    #TODO: Should work with chunk function
+    def tokenize(self, text):
+        return self.tokenizer.tokenize(text)
+
+    #TODO: Create chunk function
+    def chunk(self, text):
+        pass
 
 
 class AbstractiveSummarizationModel:
-    """MODEL_CLASSES = {
-        'BART': BARTSummarizer,
-        'T5': T5Summarizer
-    }"""
-    model_classes = ['RoBERTa', 'LegalBERT']
+    
+    model_classes = ['BART', 'T5']
+
     def __init__(self, model_type):
         self.model_type = model_type
         self.model = self.load_abstractive_model()
@@ -93,6 +112,5 @@ if __name__ == "__main__":
     #final_summary = pipeline.summarize(text)
     #print(final_summary)
     
-    extractive_model = ExtractiveSummarizationModel('LegalBERT')
+    extractive_model = ExtractiveSummarizationModel('RoBERTa')
     test = extractive_model.summarize(text)
-    print(test)
