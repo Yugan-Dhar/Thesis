452f6d192c73cc99eb07cbc21851d3ae4146e342
MikaSie <mikasie6@gmail.com>
2024-04-19 17:25:12 +0200

    added sh scripts

---
+++ scripts/run_from_ics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Change the directory to the project directory
+cd /storage/scratch/6603726/Thesis
+
+# ACtivate the virtual environment
+source /storage/scratch/6603726/Thesis/venv2/bin/activate
+
+# Run the python script
+#TODO: Add all the arguments
+
+python3.9 training.py --${}
