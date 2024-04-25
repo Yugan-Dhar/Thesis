e1b32fdf0ef9c51a1c1fa6344081f074d17e10d7
MikaSie <mikasie6@gmail.com>
2024-04-25 17:46:25 +0200

    changed run.sh script
    run.sh properly works now. Can be used as before without having to set all the slurm commands

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=thesis_tune      # Job name
+#SBATCH --time=20:0:0               # Time limit hrs:min:sec
+#SBATCH --partition=gpua100         # Partition to submit to
+#SBATCH --exclusive                 # Request nodes exclusively
+#SBATCH --output=/storage/scratch/6603726/Thesis/slurm_outputs/%j.out  # Output file
+#SBATCH --mail-type=END             # Email user when job finishes
+#SBATCH --mail-user=m.s.y.sie@students.uu.nl
+
+
+# Add arguments to the python script and run it
+/storage/scratch/6603726/Thesis/venv2/bin/python3.9 training.py "$@"
