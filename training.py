e6902a24dd86f3e2ff2c1be2a6ac13ac5715e628
MikaSie <mikasie6@gmail.com>
2024-06-06 11:29:53 +0200

    added gradient checkpointing

--- training.py
+++ training.py
@@ -564,7 +564,7 @@ if __name__ == "__main__":
         report_to = "wandb",
         logging_strategy = "epoch",
         run_name = model_id,
-        predict_with_generate = True, 
+        #predict_with_generate = True, 
         eval_accumulation_steps = 8,
         generation_max_length = gen_max_length,
         hub_model_id = f"{model_id}",
