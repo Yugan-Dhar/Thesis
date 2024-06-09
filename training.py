ef7a5f8cf45c203e68bad89461ec781fbfec0c74
root <mikasie6@gmail.com>
2024-06-09 17:51:50 +0000

    Auto commit after running with RoBERTa, LongT5, dependent

--- training.py
+++ training.py
@@ -597,9 +597,9 @@ if __name__ == "__main__":
         bf16= args.bf16,
         ddp_find_unused_parameters = args.ddp_find_unused_parameters
     )
-    if args.abstractive_model == 'LongT5':
-        abstractive_model._set_gradient_checkpointing(gradient_checkpointing_kwargs={"use_reentrant": False})
-    # Defin ethe data collator
+    """if args.abstractive_model == 'LongT5':
+        abstractive_model._set_gradient_checkpointing(gradient_checkpointing_kwargs={"use_reentrant": False})"""
+    # Define the data collator
     data_collator = DataCollatorForSeq2Seq(abstractive_tokenizer, model = abstractive_model)
 
     # Create the trainer
