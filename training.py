9bb862799e25de920fc35c734da61393cf1e8b24
root <mikasie6@gmail.com>
2024-06-10 07:31:32 +0000

    Auto commit after running with RoBERTa, LongT5, dependent

--- training.py
+++ training.py
@@ -597,7 +597,7 @@ if __name__ == "__main__":
 
     if args.abstractive_model == 'LongT5':
         print("LongT5 model detected. Adjusting training arguments for LongT5 model.")
-        training_args.ddp_find_unused_parameters = True,
+        training_args.ddp_find_unused_parameters = True
         training_args.gradient_checkpointing_kwargs= {'use_reentrant': True}
 
 
