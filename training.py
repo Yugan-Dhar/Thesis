b7d960b607e5a4cef78258d8e334710ef130dc03
root <mikasie6@gmail.com>
2024-06-10 06:57:13 +0000

    Auto commit after running with RoBERTa, LongT5, dependent

--- training.py
+++ training.py
@@ -596,8 +596,9 @@ if __name__ == "__main__":
     )
 
     if args.abstractive_model == 'LongT5':
+        print("LongT5 model detected. Adjusting training arguments for LongT5 model.")
         training_args.ddp_find_unused_parameters = True,
-        training_args.gradient_checkpointing_kwargs= {'use_reentrant':False},
+        training_args.gradient_checkpointing_kwargs= {'use_reentrant':False}
 
 
     if args.abstractive_model == 'LLama3' or args.abstractive_model == 'Mixtral':
